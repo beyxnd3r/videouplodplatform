0f8734cadbe2e96edab7e8976b1c23e50c12bf71
Gleb Gordeev <beyond3rlol@gmail.com>
2025-05-01 21:58:32 +0700

    Delete videos directory

--- videos/admin.py
+++
@@ -1,8 +0,0 @@
-# videos/admin.py
-
-from django.contrib import admin
-from .models import Video
-
-@admin.register(Video)
-class VideoAdmin(admin.ModelAdmin):
-    list_display = ['title', 'description', 'user', 'created_at']
